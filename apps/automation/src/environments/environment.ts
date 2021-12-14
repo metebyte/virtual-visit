db954353e749545c222a85c77731ebc2bfa44ebd
Metehan Celenk <mete@metebyte.net>
2021-12-14 21:33:15 +0300

    Updated

--- apps/automation/src/environments/environment.ts
+++ apps/automation/src/environments/environment.ts
@@ -4,10 +4,10 @@ export const environment = {
   PMTEP_LOGIN_URL: 'https://pmtep.com/Login',
   PMTEP_DEALERS_URL: 'https://pmtep.com/Pos/contact',
   PMAKTIF_BASE_URL: 'https://pmaktif.com/auth/login/phone-number',
-  PMAKTIF_PHONE_NUMBER: '5313911252',
-  PMAKTIF_PASSWORD: '5313911252.',
+  PMAKTIF_PHONE_NUMBER: '5459791726',
+  PMAKTIF_PASSWORD: '5459791726.',
   PMTEP_EMAIL_ADRESS: 'safak.celenk@atesgrup.com.tr',
   PMTEP_PASSWORD: '5399120273.',
   PMTEP_REMEMBER_ME: true,
-  PMTEP_TRANSPORTER_CONTACT_PHONE_NUMBER: '5313911252',
+  PMTEP_TRANSPORTER_CONTACT_PHONE_NUMBER: '5459791726',
 }
